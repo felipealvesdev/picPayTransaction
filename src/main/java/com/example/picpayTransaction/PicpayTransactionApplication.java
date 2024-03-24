e4451438030fb547458d2f908d9c7df48e1268ea
Felipe Alves <felipealexandre1408@gmail.com>
2024-03-24 11:05:21 -0300

    First commit

---
+++ src/main/java/com/example/picpayTransaction/PicpayTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.example.picpayTransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/picpayTransaction/PicpayTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.picpayTransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
